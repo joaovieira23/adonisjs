d2d8f662b60cf71ae3bb266a01e11854c2909006
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-04-30 17:41:30 -0300

    feat: add relationships between models

--- app/Models/User.js
+++ app/Models/User.js
@@ -34,6 +34,14 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
+  }
+
+  task () {
+    return this.hasMany('App/Models/Task')
+  }
 }
 
 module.exports = User
